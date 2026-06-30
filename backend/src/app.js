232f940c97abc4c93a3362da6e85f43053d2c4f5
Amanpreet Kaur Garcha <akgarcha19@gmail.com>
2026-06-30 23:50:38 +0530

    Setup Express server and MongoDB connection

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		credentials: true,
+	}),
+);
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+	res.json({
+		success: true,
+		message: "Welcome to ClubVerse API",
+	});
+});
+
+module.exports = app;
